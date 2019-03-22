ddb2613b38573cf27e5a5fd12011efeb89fb35fd
Felix Castro P <fcatdeveloper@gmail.com>
2019-03-22 15:51:37 -0500

    Solucion de la pagina

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+/**Escribe un programa que use console.log para imprimir todos los números del 1 al 100, con dos excepciones. 
+ * Para números divisibles por 3, imprime "Fizz" en vez del número y para números divisibles por 5 (pero no por 3), 
+ * imprime "Buzz".
+ * 
+ * Cuando tengas eso funcionando, modifica tu programa para imprimir "FizzBuzz", 
+ * para números que sean divisibles tanto por 3 como por 5 
+ * (y que siga imprimiendo "Fizz" o "Buzz" para números divisibles por sólo uno de ellos). */
+
+
+for (let numero = 1; numero <= 100; numero++) {
+    
+    let salida = "";
+
+    if (numero % 3 == 0) salida += "Fizz";
+
+    if (numero % 5 == 0) salida += "Buzz";
+
+    console.log(salida || numero);    
+
+}
